201466d31140b9a01255e5fef035146848051f5e
Pranav Dixit <pranavdixit20@gmail.com>
2025-06-01 12:04:10 +0530

    version='0.1.2'

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='django-realtime-logs',
-    version='0.1.1',
+    version='0.1.2',
     description='Realtime log viewer for Django using Channels',
     long_description=README,
     long_description_content_type='text/markdown',
